fa457c1742177d784d12278bc4d99d9a45300a6d
discobreak21 <bastianbaer@hotmail.com>
2022-01-08 15:28:30 +0100

    updating overlap value works

--- basbaer.com/public_html/database/index.php
+++ basbaer.com/public_html/database/index.php
@@ -1,12 +1,14 @@
 <?php
     //some variables
-    $table = "Festivals";
+    $table_name = "Festivals";
     $col_name= "name";
     $col_start= "start";
     $col_end= "end";
+    $col_overlap= "overlap";
     $col_loc= "location";
     $col_tic= "tickets";
     $col_inf= "info";
+    $number_of_cols = 8;
 
     //get the server and the db name (db name and username are the same) from the stack cp control panel > MySQL Databases
     //Note: the password does not contain special characters
@@ -20,7 +22,7 @@
         die("Connection Error: Maybe the password contains special characters");
     }
 
-    $query = "SELECT * FROM $table";
+    $query = "SELECT * FROM $table_name ORDER BY $col_start";
 
     $result = mysqli_query($link, $query);
 
@@ -34,19 +36,28 @@
                 <th>Tickets</th>
                 <th>Infos</th>
             </tr>";
+    
+    //
+    $comp_date;
 
     while($row = mysqli_fetch_array($result)){
 
         $table = $table."<tr>";
 
-        for ($i = 1; $i < 7; $i++){
+        for ($i = 1; $i < $number_of_cols; $i++){
 
             $output = $row[$i];
 
             //formate date correctly
             if ($i == 2 || $i == 3){
-                $date = date_create($output);
-                $output = date_format($date, "d.m.");
+                //handle festivals without a date
+                if ($output == "0000-00-00"){
+                    $output = "";
+                }else{
+                    $date = date_create($output);
+                    $output = date_format($date, "d.m.");
+                }
+
             }
 
             $table = $table."<td>".$output."</td>";
@@ -78,7 +89,7 @@
         $inf = $_POST['inf'];
 
         //check if festival already exists
-        $query = "SELECT id FROM $table WHERE $col_name = '".mysqli_real_escape_string($link, $name)."';";
+        $query = "SELECT id FROM $table_name WHERE $col_name = '".mysqli_real_escape_string($link, $name)."';";
 
         $result = mysqli_query($link, $query);
 
@@ -88,11 +99,45 @@
             
         }else{
             //Festival is not in db
+
+            //reformat the dates
+            $start= date_create($start);
+            $start= date_format($start, "Y-m-d");
+
+            $end= date_create($end);
+            $end= date_format($end, "Y-m-d");
+
+            
+            //overlap variable
+            $overlap= 0;
+            //check if there is a overlap with a other festival
+            $query = "SELECT id FROM $table_name WHERE $col_start BETWEEN '".mysqli_real_escape_string($link, $start)."' AND '"
+            .mysqli_real_escape_string($link, $end)."' OR $col_end BETWEEN '".mysqli_real_escape_string($link, $start)."' AND '"
+            .mysqli_real_escape_string($link, $end)."' OR ($col_start < '".mysqli_real_escape_string($link, $start)."' AND $col_end > '"
+            .mysqli_real_escape_string($link, $end)."')";
+
+
+
+            $overlap_festivals = mysqli_query($link, $query);
+
+            if($overlap_festivals){
+                while($row = mysqli_fetch_array($overlap_festivals)){
+                    //change overlap to true
+                    $query = "UPDATE $table_name SET $col_overlap = '1' WHERE id = '".$row[0]."' LIMIT 1";
+                    mysqli_query($link, $query);
+                    $overlap = 1;
+                }
+                
+            }else{
+                echo "no overlaps";
+            }
+
             //Add the festival
-            $insert = "INSERT INTO $table (id, $col_name, $col_start, $col_end, $col_loc, $col_tic, $col_inf) VALUES (NULL, '"
+            $insert = "INSERT INTO $table_name (id, $col_name, $col_start, $col_end, $col_overlap, $col_loc, $col_tic, $col_inf) VALUES (NULL, '"
             .mysqli_real_escape_string($link, $name)."', '"
             .mysqli_real_escape_string($link, $start)."', '"
             .mysqli_real_escape_string($link, $end)."', '"
+            .$overlap."', '"
             .mysqli_real_escape_string($link, $loc)."', '"
             .mysqli_real_escape_string($link, $tic)."', '"
             .mysqli_real_escape_string($link, $inf)."')";
@@ -182,6 +227,9 @@
 </html>
 
 to do:
-change date appearance
 change color if the same date
-order by date 
+            - change db
+            - change inserting into db
+            - change 
+passwort hinzufuegen
+display links nicely
