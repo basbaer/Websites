283f0b89f9a3a1b05ab2ab56257868b06ba98094
discobreak21 <bastianbaer@hotmail.com>
2022-01-29 02:54:02 +0100

    up and running

--- basbaer.com/public_html/index.php
+++ basbaer.com/public_html/index.php
@@ -72,7 +72,7 @@
 
     $error .= "Session: ".$_SESSION['admin']."<br>";
 
-    echo $error;
+    //echo $error;
 
 
 ?>
@@ -91,13 +91,13 @@
 
     <style>
         body {
-            margin: 0;
+
             font-family: 'M PLUS 1 Code', sans-serif;
-            color: white;
-            background: #FA7268;
+            background-image: url("pictures/bg_2.JPG");
+            margin: 0;
             overflow-x: hidden; /* no scrolling is possible */
             overflow-y: hidden;
-
+            
         }
 
         main {
@@ -114,53 +114,14 @@
             
         }
 
-        section {
-            position: relative;
-            z-index: 1;
+        .center_text{
+            text-align: center;
+            color: white;
         }
 
-        .center_text {
-            text-align:center;
-        }
-
-        .flip {
-            transform: rotate(180deg);
-
-        }
-
-        .blob_motion {
-            position: absolute;
-            z-index: 1;
-        }
-
-        .blob_content {
-            z-index: 2;
-            display:flex;
-            justify-content: center;
-            margin-top: 25vh;
-            margin-left: 45vw;
-            position:absolute;
-            align-items: center;
-
-            /*
-            
-            width: 50vw;
-            height: 20vh;
-            top: 40vh;
-            left: 25vw;
-            z-index: 2; /* makes it appear over blob-motion because of the higher z-index */
-        }
-
-        #top_waves {
-            position: fixed;
-            top: 0;
-            z-index: 0;
-        }
-
-        #bottom_waves {
-            position: fixed;
-            bottom: 0;
-            z-index: 0;
+        #wrap{
+            margin-top: 20vh;
+            text-align: center;
         }
     </style>
 
@@ -169,43 +130,17 @@
 
 <body>
 
+           
+    <div id="wrap">
+        <h2 class="center_text">Log In</h2>
 
-    <section>
-            <svg class="blob_motion" id="visual" viewBox="0 0 900 300" width="100vw" height="auto"
-                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
-                <g transform="translate(448.5426058915106 143.74371554266904)">
-                    <path id="blob_0"
-                        d="M63.7 -54.8C88.7 -38.7 119.3 -19.3 118.5 -0.8C117.7 17.7 85.4 35.4 60.4 56.7C35.4 78 17.7 103 -3.8 106.8C-25.2 110.6 -50.4 93.1 -71.9 71.8C-93.4 50.4 -111.2 25.2 -114.9 -3.7C-118.5 -32.5 -108.1 -65.1 -86.6 -81.2C-65.1 -97.4 -32.5 -97.2 -6.6 -90.6C19.3 -84 38.7 -71 63.7 -54.8"
-                        fill="#C62368"></path>
-                </g>
-                <g transform="translate(458.38458696758266 141.53148459473746)" style="visibility: hidden">
-                    <path id="blob_1"
-                        d="M80.6 -78.8C99.9 -61.3 108 -30.6 109.3 1.3C110.6 33.2 105.1 66.5 85.8 86.8C66.5 107.1 33.2 114.6 0.7 113.9C-31.8 113.2 -63.6 104.3 -88.6 84C-113.6 63.6 -131.8 31.8 -124.7 7.1C-117.7 -17.7 -85.4 -35.4 -60.4 -52.9C-35.4 -70.4 -17.7 -87.7 6.5 -94.2C30.6 -100.6 61.3 -96.3 80.6 -78.8"
-                        fill="#C62368"></path>
-                </g>
-            </svg>
+        <form method="post" id="form_password">
+            <input name="password" type="password" id="passwordLogIn">
+        </form>
 
-            <div class="blob_content">
-                <div>
-                    <h2 class="center_text">Log In</h2>
-
-                    <form method="post" id="form_password">
-                        <input name="password" type="password" id="passwordLogIn">
-                    </form>
-
-                </div>
-            </div>
+    </div>
 
 
-            <script>
-                const tween = KUTE.fromTo(
-                    '#blob_0',
-                    { path: '#blob_0' },
-                    { path: '#blob_1' },
-                    { repeat: 999, duration: 3000, yoyo: true }
-                ).start();
-            </script>
-        </section>
 
     <script type="text/javascript">
         // Get the input field
