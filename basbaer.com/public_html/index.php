7409de81920bf76e0f697bce7d3ce1205501153a
discobreak21 <bastianbaer@hotmail.com>
2023-03-14 11:59:59 +0100

    update

--- basbaer.com/public_html/index.php
+++ basbaer.com/public_html/index.php
@@ -1,3 +1,136 @@
+
+
+<html>
+
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bas Baer</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1+Code&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/kute.js@2.1.2/dist/kute.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous" >
+
+
+
+    <style>
+        body {
+
+            font-family: 'M PLUS 1 Code', sans-serif;
+            background-image: url("pictures/bg_2.jpg");
+            background-size: cover;
+            margin: 0;
+            overflow-x: hidden; /* no scrolling is possible */
+            overflow-y: hidden;
+            
+        }
+
+        main {
+            width: 100vw;
+            height: 100vh;
+            display: flex;
+            align-items: center;
+        }
+
+
+
+        h1 {
+            font-size: 3vmin; /* font size relative to the viewport */
+            
+        }
+
+        .center_text{
+            text-align: center;
+            color: white;
+        }
+
+        .hidden_at_start{
+            visibility: hidden;
+        }
+
+        #wrap{
+            margin-top: 20vh;
+            text-align: center;
+        }
+
+        #pw_incorrect{
+            color: white;
+        }
+
+
+
+
+    </style>
+
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="festivals/">Festivals</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="food/">Calendar</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+            
+
+           
+    <div id="wrap">
+        <h2 class="center_text">Log In</h2>
+
+        <form method="post" id="form_password">
+            <input class="form-control mx-auto w-25" type="text" placeholder="Password" id="passwordLogIn" name="password">
+        </form>
+
+        <div class="container">
+            <p class="hidden_at_start" id="pw_incorrect">Password is incorrect</p>
+        </div>
+
+    </div>
+
+
+
+    <script type="text/javascript">
+        // Get the input field
+        var input = document.getElementById("passwordLogIn");
+
+        // Execute a function when the user releases a key on the keyboard
+        input.addEventListener("keyup", function(event) {
+        // Number 13 is the "Enter" key on the keyboard
+            if (event.keyCode === 13) {
+                // Cancel the default action, if needed
+                event.preventDefault();
+                // Trigger the button element with a click
+                //document.getElementById("ButtonLogIn").click();
+                function submitform(){
+                    document.getElementById("form_password").submit();
+                }
+            }
+        }); 
+
+    </script>
+
+</body>
+
+
+
+</html>
+
 <?php
 
     session_start();
@@ -9,7 +142,8 @@
 
     //get the server and the db name (db name and username are the same) from the stack cp control panel > MySQL Databases
     //Note: the password does not contain special characters
-    $link = mysqli_connect("sdb-f.hosting.stackcp.net", "logInDb-3138340968", "ud0uz58bam", "logInDb-3138340968");
+    //$link = mysqli_connect("sdb-f.hosting.stackcp.net", "logInDb-3138340968", "ud0uz58bam", "logInDb-3138340968");
+    $link = mysqli_connect("mysql.stackcp.com", "logInDb-3138340968", "ud0uz58bam", "logInDb-3138340968", "58438");
 
     //this line is needed to display special characters properly
     $link->query("SET NAMES 'utf8'");
@@ -62,6 +196,19 @@
 
 
                     }else{
+
+                        //show password is not correct
+                        ?>
+                        <script type="text/javascript">
+                            // Get the input field
+                            var pw_inc = document.getElementById("pw_incorrect");
+
+                            pw_inc.style.visibility= "visible";
+
+                        </script>
+
+                        <?php
+
                         $error.= "password does not match<br>";
                     }
                 }
@@ -78,124 +225,8 @@
         $error .= "No POST variable<br>";
     }
 
-    $error .= "Session: ".$_SESSION['admin']."<br>";
 
     //echo $error;
 
 
 ?>
-
-<html>
-
-<head>
-<meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bas Baer</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1+Code&display=swap" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/kute.js@2.1.2/dist/kute.min.js"></script>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous" >
-
-
-
-    <style>
-        body {
-
-            font-family: 'M PLUS 1 Code', sans-serif;
-            background-image: url("pictures/bg_2.jpg");
-            background-size: cover;
-            margin: 0;
-            overflow-x: hidden; /* no scrolling is possible */
-            overflow-y: hidden;
-            
-        }
-
-        main {
-            width: 100vw;
-            height: 100vh;
-            display: flex;
-            align-items: center;
-        }
-
-
-
-        h1 {
-            font-size: 3vmin; /* font size relative to the viewport */
-            
-        }
-
-        .center_text{
-            text-align: center;
-            color: white;
-        }
-
-        #wrap{
-            margin-top: 20vh;
-            text-align: center;
-        }
-    </style>
-
-
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-
-            <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav mx-auto">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="">Home</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="festivals/">Festivals</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="food/">Calendar</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-    </nav>
-            
-
-           
-    <div id="wrap">
-        <h2 class="center_text">Log In</h2>
-
-        <form method="post" id="form_password">
-            <input name="password" type="password" id="passwordLogIn">
-        </form>
-
-    </div>
-
-
-
-    <script type="text/javascript">
-        // Get the input field
-        var input = document.getElementById("passwordLogIn");
-
-        // Execute a function when the user releases a key on the keyboard
-        input.addEventListener("keyup", function(event) {
-        // Number 13 is the "Enter" key on the keyboard
-            if (event.keyCode === 13) {
-                // Cancel the default action, if needed
-                event.preventDefault();
-                // Trigger the button element with a click
-                //document.getElementById("ButtonLogIn").click();
-                function submitform(){
-                    document.getElementById("form_password").submit();
-                }
-            }
-        }); 
-
-    </script>
-
-</body>
-
-
-
-</html>
--- dj-rubin.de/public_html/booking.php
+++ dj-rubin.de/public_html/booking.php
@@ -1,75 +1,5 @@
 <?php
 
-if ($_POST) {
-
-    $error = "";
-
-    if (!$_POST["mail"]){
-
-        $error .= "An email address is required <br>";
-
-
-    }
-
-    
-
-    if (!$_POST["InputSubject"]){
-
-        $error .= "The Suject field is required <br>";
-
-    }
-
-    if (!$_POST["InputBody"]){
-
-        $error .= "The text field is required <br>";
-
-    }
-
-    if (!filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL)) {
-        $error .= "Invalid email format";
-    }
-
-
-    if ($error != ""){
-        $error = '<div class="container"><div class="alert alert-danger col-md-6 my-3" role="alert">
-        <h4 class="alert-heading">There are some errors!</h4>
-  <p>'.$error.'</p></div></div>';
-
-        echo $error;
-    }else{
-        $recipients = array("dj_rubin_official@outlook.de", "noah.gohlke@web.de");
-
-        $emailTo = implode(',', $recipients);
-
-        $subject = $_POST["InputSubject"];
-
-        //PHP_EOL breaks line   
-        $body = $_POST["InputBody"].PHP_EOL.PHP_EOL."Absender: ".$_POST["mail"];
-
-        $headers = "From: kontakt@dj-rubin.de";
-
-
-
-        if (mail($emailTo, $subject, $body, $headers)){
-
-            echo '<div class="container container_after_navbar_spaced">
-                    <div class="alert alert-success col-md-6 my-3" role="alert">
-                        <h4 class="alert-heading">Anfrage wurde gesendet</h4>
-                    </div>
-                </div>';
-
-        }else{
-            echo '<div class="container container_after_navbar_spaced">
-            <div class="alert alert-attention col-md-6 my-3" role="alert">
-                <h4 class="alert-heading">Anfrage wurde nicht gesendet (Server-Fehler)</h4>
-            </div>
-        </div>';
-        }
-
-    }
-
-}
-
 
 ?>
 <html lang="en">
@@ -80,14 +10,17 @@ if ($_POST) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
     <title>DJ Rubin | Booking & Kontakt</title>
 
     <link rel="stylesheet" type="text/css" href="style.css">
 
     <link rel="icon" href="pictures/Logo/DJR_Aufkleber_weiß_oS_oH_scaled.png" type="image/icon type">
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+    <script src="https://www.google.com/recaptcha/api.js?render=6LfIhssiAAAAAOvAWGXlrATx5wvPKOeH0IEdJsXv"></script>
 
 
 </head>
@@ -107,9 +40,7 @@ if ($_POST) {
                 <img src="pictures/Logo/DJR_Aufkleber_weiß_oS_oH.png" id="navbar_logo">
             </a>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -121,7 +52,7 @@ if ($_POST) {
                         <a class="nav-link" href="explosion.html">The Explosion</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="booking.html">Booking & Kontakt</a>
+                        <a class="nav-link active" href="booking.php">Booking & Kontakt</a>
                     </li>
 
 
@@ -138,30 +69,27 @@ if ($_POST) {
 
         <h1>Kontaktier mich!</h1>
 
-        <form class="needs-validation" method="post" novalidate>
+        <form class="needs-validation" method="post" novalidate id="submitForm" action="form_submitted.php" onSubmit="return validatForm()">
 
             <div class="col-md-6 mb-3 has-validation">
+
                 <label for="InputEmail" class="form-label">Deine E-Mail</label>
-                <input type="email" class="form-control" id="InputEmail" name="mail" aria-describedby="emailHelp"
-                    required>
+
+                <input type="email" class="form-control" id="InputEmail" name="mail" aria-describedby="emailHelp" required>
                 <div class="invalid-feedback">
                     Bitte gib eine gültige E-Mail-Adresse ein.
                 </div>
 
                 <div id="emailHelp" class="form-text">Deine E-Mail Adresse wird lediglich zur Kontaktaufnahme verwendet</div>
 
-
-
-
-
             </div>
 
             <div class="col-md-6 mb-3">
 
-                <label for="InputSubject" class="form-label">Betreff</label>
-                <input type="text" class="form-control" name="InputSubject" id="InputSubject" required>
+                <label for="InputName" class="form-label">Name/Organisation</label>
+                <input type="text" class="form-control" name="InputName" id="InputName" required>
                 <div class="invalid-feedback">
-                    Bitte gib einen Betreff an.
+                    Bitte gib an, wer du bist.
                 </div>
 
 
@@ -173,7 +101,7 @@ if ($_POST) {
                 <label for="InputBody" class="form-label">Deine Nachricht</label>
                 <textarea class="form-control" id="InputBody" name="InputBody" rows="4" required></textarea>
                 <div class="invalid-feedback">
-                    Bitte teile uns mit, warum die die Anfrage schickst.
+                    Bitte teile uns mit, warum du die Anfrage schickst.
                 </div>
 
 
@@ -181,10 +109,45 @@ if ($_POST) {
             </div>
 
             <div class="col-12">
-                <button class="btn btn-dark" type="submit">Anfrage absenden!</button>
+                <button class="btn btn-dark" value="submit" type="submit">Anfrage absenden!</button>
             </div>
 
         </form>
+
+        <script>
+            function validateForm(){
+                var emails_fmt =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+                let email = document.forms["submitForm"]["mail"].value;
+                let name = document.forms["submitForm"]["InputName"].value;
+                let body = document.forms["submitForm"]["InputBody"].value;
+                if (email == "") {
+                    return false;
+                } else if (!(email.match(emails_fmt))){
+                    return false;
+                } else if (name == ""){
+                    return false;
+                } else if (body == ""){
+                    return false;
+                }
+            }
+
+            $('#submitForm').submit(function(event) {
+                event.preventDefault();
+                var email = $('#InputEmail').val();
+                var subject = $('#InputName').val();
+                var body = $('#InputBody').val();
+
+                grecaptcha.ready(function() {
+                    grecaptcha.execute('6LfIhssiAAAAAOvAWGXlrATx5wvPKOeH0IEdJsXv', {
+                        action: 'submit'
+                    }).then(function(token) {
+                        $('#submitForm').prepend('<input type="hidden" name="token" value="' + token + '">');
+                        $('#submitForm').prepend('<input type="hidden" name="action" value="submit">');
+                        $('#submitForm').unbind('submit').submit();
+                    });;
+                });
+            });
+        </script>
 
 
 
@@ -196,61 +159,56 @@ if ($_POST) {
 
     <!--Footer-->
     <footer>
-      <hr class="mt-5">
+        <hr class="mt-5">
 
-      <div class="container mb-5">
+        <div class="container mb-5">
 
-        <div class="row">
+            <div class="row">
 
-          <div class="col-4 d-flex justify-content-start">
-            <div class="my-auto mx-auto">
-              <a href="https://www.instagram.com/dj_rubin_official/" target="_blank">
-                <img src="pictures/Icons/Instagram_icon.png" class="icon">
-              </a>
+                <div class="col-4 d-flex justify-content-start">
+                    <div class="my-auto mx-auto">
+                        <a href="https://www.instagram.com/dj_rubin_official/" target="_blank">
+                            <img src="pictures/Icons/Instagram_icon.png" class="icon">
+                        </a>
+                    </div>
+                    <div class="my-auto mx-auto">
+                        <a href="https://www.facebook.com/rubinDJ/" target="_blank">
+                            <img src="pictures/Icons/facebook_icon.png" class="icon">
+                        </a>
+                    </div>
+
+                    <div class="my-auto mx-auto">
+                        <a href="https://soundcloud.com/user-999693809-597950715" target="_blank">
+                            <img src="pictures/Icons/soundcloud.png" class="icon">
+                        </a>
+                    </div>
+
+                    <div class="my-auto mx-auto">
+                        <a href="mailto: dj_rubin_official@outlook.de">
+                            <img src="pictures/Icons/mail.svg" class="icon">
+                        </a>
+                    </div>
+
+
+                </div>
+
+                <div class="col-8 text-end my-auto" id="footer_text">
+                    <a href="datenschutz.html" class="footer_link">Datenschutz</a> | <a href="impressum.html" class="footer_link">Impressum</a>
+                </div>
+
             </div>
-            <div class="my-auto mx-auto">
-              <a href="https://www.facebook.com/rubinDJ/" target="_blank">
-                <img src="pictures/Icons/facebook_icon.png" class="icon">
-              </a>
-            </div>
-
-            <div class="my-auto mx-auto">
-              <a href="https://soundcloud.com/user-999693809-597950715" target="_blank">
-                <img src="pictures/Icons/soundcloud.png" class="icon">
-              </a>
-            </div>
-
-            <div class="my-auto mx-auto">
-              <a href="mailto: dj_rubin_official@outlook.de">
-                <img src="pictures/Icons/mail.svg" class="icon">
-              </a>
-            </div>
-
-
-          </div>
-
-          <div class="col-8 text-end my-auto" id="footer_text">
-            <a href="datenschutz.html" class="footer_link">Datenschutz</a> | <a href="impressum.html"
-              class="footer_link">Impressum</a>
-          </div>
 
         </div>
 
-      </div>
-
     </footer>
 
-    </div>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
     <script type="text/javascript">
-
         // Example starter JavaScript for disabling form submissions if there are invalid fields
-        (function () {
+        (function() {
             'use strict'
 
             // Fetch all the forms we want to apply custom Bootstrap validation styles to
@@ -258,8 +216,8 @@ if ($_POST) {
 
             // Loop over them and prevent submission
             Array.prototype.slice.call(forms)
-                .forEach(function (form) {
-                    form.addEventListener('submit', function (event) {
+                .forEach(function(form) {
+                    form.addEventListener('submit', function(event) {
                         if (!form.checkValidity()) {
                             event.preventDefault()
                             event.stopPropagation()
@@ -269,8 +227,6 @@ if ($_POST) {
                     }, false)
                 })
         })()
-
-
     </script>
 
 </body>
