d1ba495bde4b50927b02c7269e6f683da61fd3eb
Bastian Bär <bastianbaer@hotmail.com>
2023-12-28 21:12:21 +0100

    Update to fixText at /rosa

--- basbaer.com/public_html/rosa/index.php
+++ basbaer.com/public_html/rosa/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Love u:)</title>
+    <title>Danke</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Blaka&display=swap" rel="stylesheet"> 
@@ -17,14 +17,16 @@
             font-family: 'M PLUS 1 Code', sans-serif;
             align-items: center;
             margin: 0;
-            overflow-x: hidden; /* no scrolling is possible */
+           /* overflow-x: hidden; *//* no scrolling is possible */
             overflow-y: hidden;
             
         }
 
-        h1 {
+        h2 {
             text-align:center;
-            margin-top: 30vh;
+           /* margin-top: 30vh;*/
+            margin-top: 10vh;
+            margin-bottom: 10vh;
             font-size:x-large;
             font-family: 'Bebas Neue', cursive;
         }
@@ -127,29 +129,43 @@ function get_row_of_the_day(){
 //get the server and the db name (db name and username are the same) from the stack cp control panel > MySQL Databases
 //Note: the password does not contain special characters
 //$link = mysqli_connect("mysql.stackcp.com", "ownstuffdb-313235581b", "lOdyA4LhqQD", "ownstuffdb-313235581b", "58626");
-$link = mysqli_connect("shareddb-s.hosting.stackcp.net", "ownstuffdb-313235581b", "lOdyA4LhqQD", "ownstuffdb-313235581b");
+#$link = mysqli_connect("shareddb-s.hosting.stackcp.net", "ownstuffdb-313235581b", "lOdyA4LhqQD", "ownstuffdb-313235581b");
 
 //this line is needed to display special characters properly
-$link->query("SET NAMES 'utf8'");
+#$link->query("SET NAMES 'utf8'");
 
 // this will echo nothing if there is no error
-if (mysqli_connect_error()){
-    die("Connection Error: Maybe the password contains special characters");
-}
+#if (mysqli_connect_error()){
+#    die("Connection Error: Maybe the password contains special characters");
+#}
 
-$row = get_row_of_the_day();
+#$row = get_row_of_the_day();
 
-$spotify_link = $row[$col_link];
+#$spotify_link = $row[$col_link];
+
+// $page = '<div class="container-fluid align-items-center">
+//             <div class="container align-items-center">
+//                 <h1 id="lyric" onclick="open_spotify()">"'.$row[$col_lyric].'"</h1>
+//             </div>
+//             <div class="container align-items-center">
+//                 <p>- aus "'.$row[$col_song].'" von '.$row[$col_artist].'</p>
+//             </div>
+//         </div>
+//         ';
+
+$fixText = "Hey, Rosa.
+Ich wollte dir noch einmal 'Danke' sagen. Die eineinhalb Jahre waren für mich eine wunderschöne Zeit. Ich bin dankbar für all die unvergesslichen Momente, die wir zusammen erleben durften. Du bist und bleibst ein toller Mensch und ich weiß es sehr zu schätzen, dass wir einen Teil unsers Lebens auf so eine enge,intime Weise verbringen konnten. Ich weiß es auch zu schätzen, wie sehr du mich geliebt und um uns gekämpft hast. Es tut mir leid, dass ich das gegen Ende nicht getan habe. Du hast so etwas nicht verdient. Es tut mir leid, dass ich mich nicht besser öffnen konnte und den Rückzug gesucht habe. Es tut mir leid, dass ich nicht so für dich da sein konnte, wie ich es mir von mir selbst gewünscht hätte.
+Ich habe durch dich und unsere Beziehung sehr viel über mich und das Leben gelernt. Ich bin sehr dankbar über die vielfältigen Situationen, in denen du mir neue Perspektiven gezeigt hast und mich zu einem besseren Menschen gemacht hast.
+Für mich bleibst du immer ein besonderer Mensch, zu dem ich eine besondere Verbinung habe.";
 
 $page = '<div class="container-fluid align-items-center">
             <div class="container align-items-center">
-                <h1 id="lyric" onclick="open_spotify()">"'.$row[$col_lyric].'"</h1>
+                <h2 id="lyric">'.$fixText.'</h2>
             </div>
-            <div class="container align-items-center">
-                <p>- aus "'.$row[$col_song].'" von '.$row[$col_artist].'</p>
-            </div>
-        </div>
+            
         ';
+
+
 
 
 echo $page;
